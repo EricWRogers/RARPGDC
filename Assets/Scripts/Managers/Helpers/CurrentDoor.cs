960a74cb35f22633714f3c32f24a5ae7c65df765
Scott Schroeder <schrosam@gmail.com>
2026-08-21 19:50:01 -0500

    Lock camera in small rooms

---
+++ Assets/Scripts/Managers/Helpers/CurrentDoor.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class CurrentDoor : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //FindFirstObjectByType<RoomManager>().currentDoor = gameObject;
+    }
+}
