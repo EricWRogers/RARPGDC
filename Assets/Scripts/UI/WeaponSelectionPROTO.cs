ffb499fd8f36769b4bf408db822d5333d83c0d3e
Scott Schroeder <schrosam@gmail.com>
2026-08-26 13:54:40 -0500

    Began fireball staff
    Looks cool, doesn't do anything

--- Assets/Scripts/UI/WeaponSelectionPROTO.cs
+++ Assets/Scripts/UI/WeaponSelectionPROTO.cs
@@ -1,5 +1,3 @@
-using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 
 public class WeaponSelectionPROTO : MonoBehaviour
@@ -7,25 +5,17 @@ public class WeaponSelectionPROTO : MonoBehaviour
     public GameObject _mainMenu;
     public GameObject player;
     private SimplePlayerController playerScript; 
-    public GameObject swordPrefab;
-    public GameObject staffPrefab;
-    public GameObject macePrefab;
 
-    public List<float> defaultSword = new List<float> {2f, 0.8f, 0.1f};
-    public List<float> defaultStaff = new List<float> {4f, 0.8f, 0.3f};
-    public List<float> defaultMace = new List<float> {8f, 0.8f, 0.6f};
-    public TMP_Text swordText;
-    public TMP_Text spearText; // do we have a spear or staff? IDK
-    public TMP_Text maceText;
-    
+    // public List<float> defaultSword = new List<float> {2f, 0.8f, 0.1f};
+    // public List<float> defaultStaff = new List<float> {4f, 0.8f, 0.3f};
+    // public List<float> defaultMace = new List<float> {8f, 0.8f, 0.6f};
     
 
     //public Button _swordBtn, _spearBtn, _maceBtn, _quit;
     //public string sword, spear, mace;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-
     void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player");
         _mainMenu.SetActive(true);
         Time.timeScale = 0;
 
@@ -48,24 +38,24 @@ public class WeaponSelectionPROTO : MonoBehaviour
     {
         GameObject childWeapon = Instantiate(weaponPrefab, player.transform, false);
 
-        if(weaponPrefab.name == "Sword")
-        {
-            childWeapon.GetComponent<Weapon>().damage = (int)defaultSword[0];
-            childWeapon.GetComponent<Weapon>().attackRange = defaultSword[1];
-            childWeapon.GetComponent<Weapon>().animationStepLength = defaultSword[2];
-        }
-        else if (weaponPrefab.name == "Staff")
-        {
-            childWeapon.GetComponent<Weapon>().damage = (int)defaultStaff[0];
-            childWeapon.GetComponent<Weapon>().attackRange = defaultStaff[1];
-            childWeapon.GetComponent<Weapon>().animationStepLength = defaultStaff[2];
-        }
-        else if (weaponPrefab.name == "Mace")
-        {
-            childWeapon.GetComponent<Weapon>().damage = (int)defaultMace[0];
-            childWeapon.GetComponent<Weapon>().attackRange = defaultMace[1];
-            childWeapon.GetComponent<Weapon>().animationStepLength = defaultMace[2];
-        }
+        // if(weaponPrefab.name == "Sword")
+        // {
+        //     childWeapon.GetComponent<Weapon>().damage = (int)defaultSword[0];
+        //     childWeapon.GetComponent<Weapon>().attackRange = defaultSword[1];
+        //     childWeapon.GetComponent<Weapon>().animationStepLength = defaultSword[2];
+        // }
+        // else if (weaponPrefab.name == "Staff")
+        // {
+        //     childWeapon.GetComponent<Weapon>().damage = (int)defaultStaff[0];
+        //     childWeapon.GetComponent<Weapon>().attackRange = defaultStaff[1];
+        //     childWeapon.GetComponent<Weapon>().animationStepLength = defaultStaff[2];
+        // }
+        // else if (weaponPrefab.name == "Mace")
+        // {
+        //     childWeapon.GetComponent<Weapon>().damage = (int)defaultMace[0];
+        //     childWeapon.GetComponent<Weapon>().attackRange = defaultMace[1];
+        //     childWeapon.GetComponent<Weapon>().animationStepLength = defaultMace[2];
+        // }
 
         childWeapon.name = "Weapon";
 
--- Assets/Scripts/Weapons/Weapon.cs
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -4,6 +4,14 @@ using System.Collections.Generic;
 
 public class Weapon : MonoBehaviour
 {
+    public enum WeaponType
+    {
+        Spear,
+        Fireball,
+        Mace
+    }
+
+    public WeaponType myType;
     public int damage;
     public Transform attackPoint;
     public float attackRange = 1f;
@@ -15,12 +23,22 @@ public class Weapon : MonoBehaviour
     private Vector3 _restingLocalPosition;
     public float spearJabDistance = 0.75f;
     public GameObject weaponBody;
+    public Light weaponLight;
+    public float fireballLightIntensity = 2f;
+    public float fireballForwardDistance = 0.15f;
+    public float fireballRecoilDistance = 0.1f;
+    private Vector3 _restingBodyLocalPosition;
+    private float _restingLightIntensity;
 
     //public GameObject weaponType;
 
     void Awake()
     {
         _restingLocalPosition = transform.localPosition;
+        if (weaponBody != null)
+            _restingBodyLocalPosition = weaponBody.transform.localPosition;
+        if (weaponLight != null)
+            _restingLightIntensity = weaponLight.intensity;
     }
 
     public void Attack()
@@ -28,7 +46,41 @@ public class Weapon : MonoBehaviour
         if (_attackSequence != null && _attackSequence.IsActive()) return;
 
         //play attack animation (dotween!)
-        _attackSequence = PerformSpearAnimation();
+        if(myType == WeaponType.Spear)
+            _attackSequence = PerformSpearAnimation();
+
+        else if(myType == WeaponType.Fireball)
+            _attackSequence = PerformFireballAnimation();
+    }
+
+    Sequence PerformFireballAnimation()
+    {
+        if (weaponBody == null || weaponLight == null) return DOTween.Sequence();
+
+        float fadeDuration = Mathf.Max(animationStepLength, 0.01f);
+        Vector3 forwardPosition = _restingBodyLocalPosition + weaponBody.transform.forward * fireballForwardDistance;
+        Vector3 recoilPosition = _restingBodyLocalPosition + -weaponBody.transform.forward * fireballRecoilDistance;
+
+        weaponLight.intensity = 0f;
+        weaponLight.enabled = true;
+
+        Sequence sequence = DOTween.Sequence();
+        sequence.Append(weaponLight.DOIntensity(fireballLightIntensity, fadeDuration).SetEase(Ease.InOutQuad));
+        sequence.Join(weaponBody.transform.DOLocalMove(forwardPosition, fadeDuration).SetEase(Ease.InOutQuad));
+        // Shoot fireball here
+        sequence.Append(weaponBody.transform.DOLocalMove(recoilPosition, fadeDuration * 0.25f).SetEase(Ease.OutQuad));
+        sequence.Append(weaponBody.transform.DOLocalMove(_restingBodyLocalPosition, fadeDuration * 0.75f).SetEase(Ease.OutQuad));
+        sequence.Append(weaponLight.DOIntensity(_restingLightIntensity, fadeDuration * 0.5f));
+        sequence.AppendCallback(() => weaponLight.enabled = _restingLightIntensity > 0f);
+        sequence.OnComplete(() => _attackSequence = null);
+        sequence.OnKill(() =>
+        {
+            weaponBody.transform.localPosition = _restingBodyLocalPosition;
+            weaponLight.intensity = _restingLightIntensity;
+            weaponLight.enabled = _restingLightIntensity > 0f;
+            _attackSequence = null;
+        });
+        return sequence;
     }
 
     Sequence PerformSpearAnimation()
